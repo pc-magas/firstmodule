60b686376462435fe6a1c836540cd48101080483
pcmagas <pcmagas@disroot.org>
2023-09-08 16:43:06 +0300

    Cron works

--- app/code/MageGuide/FirstModule/Cron/DemoCron.php
+++ app/code/MageGuide/FirstModule/Cron/DemoCron.php
@@ -2,7 +2,21 @@
 
 namespace MageGuide\FirstModule\Cron;
 
+use Psr\Log\LoggerInterface;
 class DemoCron
 {
+    protected $logger;
 
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Write to system.log
+     *
+     * @return void
+     */
+    public function execute() {
+        $this->logger->info('Cron Works');
+    }
 }
