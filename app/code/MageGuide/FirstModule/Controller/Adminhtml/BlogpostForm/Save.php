7a53f39004384cb8dcf77ac1a4ad4394d3e53608
pcmagas <pcmagas@disroot.org>
2023-09-08 17:42:53 +0300

    Save by id

--- app/code/MageGuide/FirstModule/Controller/Adminhtml/BlogpostForm/Save.php
+++ app/code/MageGuide/FirstModule/Controller/Adminhtml/BlogpostForm/Save.php
@@ -4,18 +4,23 @@ namespace MageGuide\FirstModule\Controller\Adminhtml\BlogpostForm;
 
 use MageGuide\FirstModule\Model\BlogPost;
 
+use MageGuide\FirstModule\Model\ResourceModel\BlogPost\Collection;
 use Magento\Backend\App\Action;
 use Magento\Framework\Controller\ResultFactory;
 
 class Save extends Action
 {
     protected BlogPost $blogPostModel;
+    protected Collection $blogPostCollection;
+
 
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        BlogPost $blogPostModel
+        BlogPost $blogPostModel,
+        Collection $blogPostCollection
     ) {
         $this->blogPostModel = $blogPostModel;
+        $this->blogPostCollection = $blogPostCollection;
         parent::__construct($context);
     }
 
@@ -33,11 +38,19 @@ class Save extends Action
             $data=$data['blogpost'];
         }
 
-        $this->blogPostModel->setTitle($data['title']);
-        $this->blogPostModel->setContent($data['post']);
+        $blogPostModel = !empty($data['blog_post_id'])?$this->blogPostCollection->addFieldToFilter('blog_post_id',$data['blog_post_id'])->load()->getFirstItem():$this->blogPostModel;
+
+        if(empty($blogPostModel)){
+            $this->messageManager->addSuccess(__('Νο blog post model has been found'));
+            $resultRedirect->setUrl('adminblogposts/index/index');
+            return $resultRedirect;
+        }
+
+        $blogPostModel->setTitle($data['title']);
+        $blogPostModel->setPost($data['post']);
 
         try{
-            $this->blogPostModel->save();
+            $blogPostModel->save();
             $this->messageManager->addSuccess(__('The data has been saved.'));
             $resultRedirect->setUrl('adminblogposts/index/index');
         } catch (\Exception $e) {
