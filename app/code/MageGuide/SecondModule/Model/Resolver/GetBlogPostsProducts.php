53d157b3aede6dfb0556b42b3e563a8f38ed8844
pcmagas <pcmagas@disroot.org>
2023-09-12 15:54:42 +0300

    DO not re-try to fetch:

--- app/code/MageGuide/SecondModule/Model/Resolver/GetBlogPostsProducts.php
+++ app/code/MageGuide/SecondModule/Model/Resolver/GetBlogPostsProducts.php
@@ -2,12 +2,34 @@
 
 namespace MageGuide\SecondModule\Model\Resolver;
 
+use Magento\Catalog\Block\Product\ImageBuilder;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
+use \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
+
 class GetBlogPostsProducts implements ResolverInterface
 {
+
+    private ProductCollectionFactory $productCollectionFactory;
+    private ValueFactory $valueFactory;
+
+    private $skus = [];
+
+    private $results = [];
+
+    public function __construct(
+        ProductCollectionFactory $collectionFactory,
+        ValueFactory $valueFactory
+    ){
+        $this->productCollectionFactory = $collectionFactory;
+        $this->valueFactory = $valueFactory;
+    }
+
     public function resolve(   Field $field,
                                      $context,
                                ResolveInfo $info,
@@ -15,11 +37,29 @@ class GetBlogPostsProducts implements ResolverInterface
                                array $args = null)
     {
 
-        return [
-            [
-                'sku'=>1234,
-                'image'=>'https://example.com/image.png'
-            ]
-        ];
+        // No need to hydrate a model
+        $skus = $value['skus']??[];
+
+        if(empty($skus)){
+            return [];
+        }
+
+        $skus = explode(',',$skus);
+        $this->skus = array_merge($this->skus,$skus);
+        $this->skus = array_unique($this->skus);
+
+        return $this->valueFactory->create(function () {
+
+                if(!empty($this->results)){
+                    return $this->results;
+                }
+                /**
+                 * @var \Magento\Catalog\Model\ResourceModel\Product\Collection
+                 */
+                $productCollection = $this->productCollectionFactory->create();
+                $productCollection->addAttributeToSelect(['sku','image'])->getSelect()->where('sku IN (?)',$this->skus);
+                $this->results = $productCollection->load()->getItems();
+                return $this->results;
+            });
     }
 }
